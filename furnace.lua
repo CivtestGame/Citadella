6dd43e5a49f818f84ecb68b1ee24ed567c7005a8
R3R3R3 <>
2020-02-24 16:52:30 +0100

    Adapt Furnace to split inventory

--- furnace.lua
+++ furnace.lua
@@ -23,8 +23,13 @@ local function get_furnace_active_formspec(fuel_percent, item_percent)
 		"image[3.75,1.5;1,1;gui_furnace_arrow_bg.png^[lowpart:"..
 		(item_percent)..":gui_furnace_arrow_fg.png^[transformR270]"..
 		"list[context;dst;4.75,0.96;2,2;]"..
-		"list[current_player;main;0,4.25;8,1;]"..
-		"list[current_player;main;0,5.5;8,3;8]"..
+		sfinv.get_inventory_area_formspec(4.25)..
+		"listring[context;dst]"..
+		"listring[current_player;main2]"..
+		"listring[context;src]"..
+		"listring[current_player;main2]"..
+		"listring[context;fuel]"..
+		"listring[current_player;main2]"..
 		"listring[context;dst]"..
 		"listring[current_player;main]"..
 		"listring[context;src]"..
@@ -41,8 +46,13 @@ local function get_furnace_inactive_formspec()
 		"image[2.75,1.5;1,1;default_furnace_fire_bg.png]"..
 		"image[3.75,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]"..
 		"list[context;dst;4.75,0.96;2,2;]"..
-		"list[current_player;main;0,4.25;8,1;]"..
-		"list[current_player;main;0,5.5;8,3;8]"..
+		sfinv.get_inventory_area_formspec(4.25)..
+		"listring[context;dst]"..
+		"listring[current_player;main2]"..
+		"listring[context;src]"..
+		"listring[current_player;main2]"..
+		"listring[context;fuel]"..
+		"listring[current_player;main2]"..
 		"listring[context;dst]"..
 		"listring[current_player;main]"..
 		"listring[context;src]"..
