004f27d0764e50f920c302998b6e56f8c0af14f8
R3R3R3 <>
2020-04-27 01:26:24 +0200

    Add 9 new kinds of reinforcement item

--- reinforcements.lua
+++ reinforcements.lua
@@ -53,27 +53,11 @@ end
 --
 --------------------------------------------------------------------------------
 
-local function seconds(n) return n end
-local function minutes(n) return 60 * n end
-local function hours(n) return 60 * 60 * n end
-local function days(n) return 60 * 60 * 24 * n end
-
-ct.register_reinforcement_type({
-      item_name = "default:stone",
-      value = 25,
-})
-ct.register_reinforcement_type({
-      item_name = "default:copper_ingot",
-      value = 250,
-})
-ct.register_reinforcement_type({
-      item_name = "default:tin_ingot",
-      value = 250,
-})
-ct.register_reinforcement_type({
-      item_name = "default:steel_ingot",
-      value = 1800,
-})
+local function seconds(n) return               n end
+local function minutes(n) return seconds(60) * n end
+local function hours(n)   return minutes(60) * n end
+local function days(n)    return hours(24)   * n end
+local function weeks(n)   return days(7)     * n end
 
 -- Example
 -- ct.register_reinforcement_type({
@@ -91,20 +75,253 @@ ct.register_reinforcement_type({
 --       -- Reinf only becomes useful after 24h
 --       warmup_time = hours(24),
 
---       -- Reinf begins decaying after 30 days in 3h intervals
+--       -- Reinf begins decaying after 30 days over a 3h period
 --       decay_after_time = days(30),
---       decay_time_interval = hours(3),
+--       decay_length = hours(3),
 -- })
 
 -- Example
 -- ct.register_reinforcement_type({
 --       name = "Meme Reinforcement",
 --       item_name = "default:cobble",
---       value = 10,
+--       disabled = true,
 
---       -- warmup_time = minutes(2),
+--       value = 50,
+
+--       warmup_time = seconds(15),
 
 --       decay_to_value = 0,
---       decay_after_time = minutes(2),
---       decay_time_interval = seconds(30)
+--       decay_after_time = seconds(30),
+
+--       decay_length = minutes(2)
 -- })
+
+
+--------------------------------------------------------------------------------
+--
+-- Civtest Citadella reinforcements
+--
+--------------------------------------------------------------------------------
+
+-- Legacy ones: transitioning to new system
+
+ct.register_reinforcement_type({
+      item_name = "default:copper_ingot",
+      value = 250,
+      -- disabled = true,
+
+      decay_to_value = 25,
+      decay_after_time = days(30),
+      decay_length = days(30)
+})
+
+ct.register_reinforcement_type({
+      item_name = "default:tin_ingot",
+      value = 250,
+      -- disabled = true,
+
+      decay_to_value = 25,
+      decay_after_time = days(30),
+      decay_length = days(30)
+})
+
+ct.register_reinforcement_type({
+      item_name = "default:steel_ingot",
+      value = 1800,
+      -- disabled = true,
+
+      decay_to_value = 250,
+      decay_after_time = days(60),
+      decay_length = days(60)
+})
+
+--
+-- New reinforcement system
+--
+
+-- Stone
+
+ct.register_reinforcement_type({
+      item_name = "default:stone",
+      value = 25,
+      warmup_time = hours(48),
+
+      decay_to_value = 2,
+      decay_after_time = days(180),
+      decay_length = days(90)
+})
+
+-- Tin Plating
+
+minetest.register_craftitem("citadella:reinf_plating_tin", {
+	description = "Tin Plating",
+	inventory_image = "citadella_plating_tin.png",
+	groups = { reinforcement = 1 }
+})
+
+ct.register_reinforcement_type({
+      name = "Tin Plating",
+      item_name = "citadella:reinf_plating_tin",
+      value = 250,
+
+      warmup_time = hours(24),
+
+      decay_to_value = 12,
+      decay_after_time = weeks(4),
+      decay_length = weeks(4)
+})
+
+-- Bronze Rebar
+
+minetest.register_craftitem("citadella:reinf_rebar_bronze", {
+	description = "Bronze Rebar",
+	inventory_image = "citadella_rebar_bronze.png",
+	groups = { reinforcement = 1 }
+})
+
+ct.register_reinforcement_type({
+      name = "Bronze Rebar",
+      item_name = "citadella:reinf_rebar_bronze",
+      value = 150,
+      value_limit = 300,
+
+      warmup_time = hours(72),
+
+      decay_to_value = 15,
+      decay_after_time = days(180),
+      decay_length = days(90)
+})
+
+-- Grout
+
+minetest.register_craftitem("citadella:reinf_grout", {
+	description = "Grout",
+	inventory_image = "citadella_grout.png",
+	groups = { reinforcement = 1 }
+})
+
+ct.register_reinforcement_type({
+      name = "Grout",
+      item_name = "citadella:reinf_grout",
+      value = 50,
+
+      warmup_time = weeks(3),
+})
+
+-- Wrought Iron Rebar
+
+minetest.register_craftitem("citadella:reinf_rebar_wrought_iron", {
+	description = "Wrought Iron Rebar",
+	inventory_image = "citadella_rebar_wrought_iron.png",
+	groups = { reinforcement = 1 }
+})
+
+ct.register_reinforcement_type({
+      name = "Wrought Iron Rebar",
+      item_name = "citadella:reinf_rebar_wrought_iron",
+      value = 200,
+      value_limit = 400,
+
+      warmup_time = hours(72),
+
+      decay_to_value = 20,
+      decay_after_time = days(180),
+      decay_length = days(90)
+})
+
+-- Fine Steel Rebar
+
+minetest.register_craftitem("citadella:reinf_rebar_fine_steel", {
+	description = "Fine Steel Rebar",
+	inventory_image = "citadella_rebar_fine_steel.png",
+	groups = { reinforcement = 1 }
+})
+
+ct.register_reinforcement_type({
+      name = "Fine Steel Rebar",
+      item_name = "citadella:reinf_rebar_fine_steel",
+      value = 300,
+      value_limit = 1200,
+
+      warmup_time = hours(72),
+
+      decay_to_value = 60,
+      decay_after_time = days(180),
+      decay_length = days(90)
+})
+
+-- Cement
+
+minetest.register_craftitem("citadella:reinf_cement", {
+	description = "Cement",
+	inventory_image = "citadella_cement.png",
+	groups = { reinforcement = 1 }
+})
+
+ct.register_reinforcement_type({
+      name = "Cement",
+      item_name = "citadella:reinf_cement",
+      value = 75,
+
+      warmup_time = weeks(3),
+})
+
+-- Brass Plating
+
+minetest.register_craftitem("citadella:reinf_plating_brass", {
+	description = "Brass Plating",
+	inventory_image = "citadella_plating_brass.png",
+	groups = { reinforcement = 1 }
+})
+
+ct.register_reinforcement_type({
+      name = "Brass Plating",
+      item_name = "citadella:reinf_plating_brass",
+      value = 200,
+
+      warmup_time = hours(4),
+
+      decay_to_value = 10,
+      decay_after_time = weeks(2),
+      decay_after_time = weeks(1),
+})
+
+-- Stainless Steel Rebar
+
+minetest.register_craftitem("citadella:reinf_rebar_stainless_steel", {
+	description = "Stainless Steel Rebar",
+        -- TODO: citadella_rebar_stainless_steel.png
+	inventory_image = "citadella_rebar_fine_steel.png",
+	groups = { reinforcement = 1 }
+})
+
+ct.register_reinforcement_type({
+      name = "Stainless Steel Rebar",
+      item_name = "citadella:reinf_rebar_stainless_steel",
+      value = 500,
+      value_limit = 2000,
+
+      warmup_time = hours(72),
+
+      decay_to_value = 100,
+      decay_after_time = days(180),
+      decay_length = days(90)
+})
+
+-- Aluminium Bronze Plating
+
+minetest.register_craftitem("citadella:reinf_plating_alubronze", {
+	description = "Aluminium Bronze Plating",
+	inventory_image = "citadella_plating_alubronze.png",
+	groups = { reinforcement = 1 }
+})
+
+ct.register_reinforcement_type({
+      name = "Aluminium Bronze Plating",
+      item_name = "citadella:reinf_plating_alubronze",
+      value = 200,
+
+      decay_to_value = 100,
+      decay_after_time = weeks(1),
+      decay_length = days(3)
+})
