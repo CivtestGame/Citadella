847dd6aa80995a473a8101f43dfebeaf9080a1e6
CaptainKlutz <patkmccarthy@gmail.com>
2020-04-27 16:41:57 +0200

    Grout, take two

--- crafting.lua
+++ crafting.lua
@@ -1,5 +1,8 @@
 minetest.register_craft({
-	type = "shapeless",
-	output = "citadella:reinf_grout 20",
-	recipe = {"default:gravel", "default:gravel", "default:gravel", "default:gravel", "group:sand", "group:sand", "group:sand", "group:sand", "default:clay"}
+	output = 'citadella:reinf_grout 20',
+	recipe = {
+		{'default:gravel', 'default:gravel', 'default:gravel'},
+		{'default:gravel', 'group:sand', 'group:sand'},
+		{'group:sand', 'group:sand', 'default:clay'},
+	}
 })
